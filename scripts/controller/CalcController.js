dc161b7b577cdb955b499a81602a74561e55a02a
JUNIOR_OKOSTA <82908127+JUNIOROKOSTA@users.noreply.github.com>
2022-12-03 17:19:32 -0300

    att js 2
    atualizando as métodos do JS.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,36 +1,50 @@
 class CalcController{
     constructor(){
-        this._displayCalc = '0';
-        this._dateNow;
 
 
+        this._displayCalc_el = document.querySelector('.display')
+        this._date_el = document.querySelector('.data')
+        this._time_el = document.querySelector('.hora')
 
+
+        this.setDisplayDateTime();
         this.initialize();
     }
 
     initialize(){
-        let displayCalc_el = document.querySelector('.display')
-        let date_el = document.querySelector('.data')
-        let time_el = document.querySelector('.hora')
 
-        date_el.innerHTML = '02/12/2022'
-        time_el.innerHTML = '00:21'
+        setInterval(()=>{
+            this.setDisplayDateTime()
+        }, 1000)
 
+    }
+
+    setDisplayDateTime(){
+        this._date_el.innerHTML = this.dateNow
+        this._time_el.innerHTML = this.timeNow
     }
 
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalc_el.innerHTML;
     }
 
     set displayCalc(value){
-        this._displayCalc = value;
+        this._displayCalc_el.innerHTML = value;
     }
 
     get dateNow(){
-        return this._dateNow;
+        return new Date().toLocaleDateString('pt-br');
     }
 
     set dateNow(value){
         this._dateNow = value;
+    }
+
+    get timeNow(){
+        return new Date().toLocaleTimeString('pt-br');
+    }
+
+    set timeNow(value){
+        this._time_el.innerHTML = value;
     }
 }
